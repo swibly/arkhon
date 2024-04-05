e27f9a9009d2aac3ab3a67f261b9bec86a051514
devkcud <patommmmm@proton.me>
2024-04-05 10:50:29 -0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let count: number = 0;
+</script>
+
+<p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde inventore tenetur, fugit ducimus
+    ab eius incidunt sapiente optio, quia voluptatum hic dolorum quisquam ea nobis aperiam
+    laboriosam sint soluta assumenda!
+</p>
+
+<button aria-label="Counter" on:click={() => count++} class="btn">{count} clicks</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-primary">
+    {count} clicks
+</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-secondary">
+    {count} clicks
+</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-accent">{count} clicks</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-success">
+    {count} clicks
+</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-info">{count} clicks</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-warning">
+    {count} clicks
+</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-error">{count} clicks</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-neutral">
+    {count} clicks
+</button>
+<button aria-label="Counter" on:click={() => count++} class="btn btn-outline">
+    {count} clicks
+</button>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    primary: '#0175af',
+                    secondary: '#ffa333',
+                    neutral: '#000000',
+                    'base-100': '#f5f5f5',
+                    success: '#4fd35c',
+                    warning: '#ead45f',
+                    error: '#da3030'
+                },
+                dark: {
+                    primary: '#0175af',
+                    secondary: '#ffa333',
+                    neutral: '#000000',
+                    'base-100': '#000000',
+                    success: '#4fd35c',
+                    warning: '#ead45f',
+                    error: '#da3030'
+                }
+            }
+        ]
+    },
+    plugins: [require('daisyui')]
+};
