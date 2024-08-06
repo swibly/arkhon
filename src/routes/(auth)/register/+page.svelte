eb88f161135edfc468c2f00a10a777b853cb3ecb
Fantta8820 <andrevictor2512@gmail.com>
2024-08-06 20:19:37 -0300

    Trying to add the api in register page

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,8 +1,35 @@
-<script lang="ts">
+<script context="module" lang="ts">
     import Icon from '@iconify/svelte';
+    import { SwiblyClient } from 'swibly-sdk';
+
+    const client = new SwiblyClient( {key: "api-key-here"} );
+
+    if(await client.canConnect()){
+        console.log("True");
+    }else{
+        console.log("False");
+    }
 
     let showPassword: boolean = false;
     let showPasswordConfirm: boolean = false;
+    
+    let firstnameInput: HTMLInputElement;
+    let lastnameInput: HTMLInputElement;
+    let emailInput: HTMLInputElement;
+    let passwordInput: HTMLInputElement;
+    let confirm_passInput: HTMLInputElement;
+
+    function handleSubmit(e: Event){
+        e.preventDefault();
+
+        const firstname = firstnameInput.value;
+        const lastname = lastnameInput.value;
+        const email = emailInput.value;
+        const password = passwordInput.value;
+        const confirm_pass = confirm_passInput.value;
+
+        console.log(firstname);
+    }
 </script>
 
 <h1 class="text-center text-4xl sm:text-5xl px-4 font-bold text-white m-12">
@@ -14,7 +41,7 @@
 </h2>
 
 <section class="mx-12 mt-8">
-    <form>
+    <form on:submit={handleSubmit}>
         <div class="lg:flex justify-center gap-2">
             <label class="input input-bordered flex items-center gap-2 mb-4 lg:flex-1 bg-neutral">
                 <Icon icon="material-symbols:person" />
@@ -22,6 +49,7 @@
                     type="text"
                     class="firstname w-full"
                     id="firstname"
+                    bind:this={firstnameInput}
                     name="firstname"
                     placeholder="Nome"
                 />
@@ -33,6 +61,7 @@
                     type="text"
                     class="lastname w-full"
                     id="lastname"
+                    bind:this={lastnameInput}
                     name="lastname"
                     placeholder="Sobrenome"
                 />
@@ -43,7 +72,7 @@
 
         <label class="input input-bordered flex items-center gap-2 my-8 bg-neutral">
             <Icon icon="ic:baseline-email" />
-            <input type="email" class="email w-full" id="email" name="email" placeholder="Email" />
+            <input type="email" class="email w-full" id="email" bind:this={emailInput} name="email" placeholder="Email" />
         </label>
 
         <label class="input input-bordered flex items-center gap-2 mb-8 bg-neutral">
@@ -52,6 +81,7 @@
                 type={showPassword ? 'text' : 'password'}
                 class="password w-full"
                 id="password"
+                bind:this={passwordInput}
                 name="password"
                 placeholder="Senha"
             />
@@ -70,6 +100,7 @@
                 type={showPasswordConfirm ? 'text' : 'password'}
                 class="confirm_pass w-full"
                 id="confirm_pass"
+                bind:this={confirm_passInput}
                 name="confirm_pass"
                 placeholder="Confirmar Senha"
             />
@@ -92,7 +123,7 @@
         </label>
 
         <button class="text-white mt-8 pb-4 mx-auto w-fit block">
-            <a href="/home/"><Icon icon="emojione-monotone:right-arrow" font-size="60px" /></a>
+            <Icon icon="emojione-monotone:right-arrow" font-size="60px" />
         </button>
     </form>
 
