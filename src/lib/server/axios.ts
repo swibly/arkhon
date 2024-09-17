e4c3889b502d546b8b008bcf273382a447cb9eb0
André devkcud Albanese <patommmmm@proton.me>
2024-09-17 10:46:41 -0300

    fix: Create axios client

--- src/lib/server/axios.ts
+++ src/lib/server/axios.ts
@@ -1,5 +1,11 @@
 import { SWIBLY_API_BASE_URL, SWIBLY_API_KEY } from '$env/static/private';
 import axios from 'axios';
 
-axios.defaults.baseURL = SWIBLY_API_BASE_URL;
-axios.defaults.headers.common = { 'X-API-KEY': SWIBLY_API_KEY };
+export default axios.create({
+    baseURL: SWIBLY_API_BASE_URL,
+    headers: {
+        common: {
+            'X-API-KEY': SWIBLY_API_KEY
+        }
+    }
+});
