73da68ab7582d3a04e9866b18d2fc2b4a46954c6
André devkcud Albanese <patommmmm@proton.me>
2024-09-19 23:25:23 -0300

    feat: Add base layout for all authorized pages

--- src/routes/(authorized)/+layout.svelte
+++ src/routes/(authorized)/+layout.svelte
@@ -1,33 +1,41 @@
 <script lang="ts">
     import Icon from '@iconify/svelte';
+    import type { LayoutServerData } from './$types';
+
+    export let data: LayoutServerData;
 </script>
 
 <div class="min-h-screen w-full">
     <header class="flex items-center gap-2 p-4 shadow sticky top-0">
-        <a href="/home" class="text-2xl font-bold text-primary">Arkhon</a>
+        <div>
+            <a href="/home" class="text-2xl font-bold text-primary">Arkhon</a>
+            <p class="text-sm opacity-50">Bem-vindo, {data.firstname}!</p>
+        </div>
 
         <div class="grow" />
 
         <label class="swap swap-rotate">
             <input type="checkbox" class="theme-controller" value="dark" />
 
-            <Icon icon="ph:sun-fill" class="swap-off size-6" />
-            <Icon icon="ph:moon-fill" class="swap-on size-6" />
+            <Icon icon="ph:sun-fill" class="swap-off size-8" />
+            <Icon icon="ph:moon-fill" class="swap-on size-8" />
         </label>
 
         <div class="size-8 dropdown dropdown-end">
-            <button tabindex="0" class="avatar">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                    alt=""
-                    class="rounded-full"
-                />
+            <button tabindex="0">
+                <img src={data.image_url} alt="" class="rounded-full size-8" />
             </button>
 
             <ul
                 tabindex="-1"
                 class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
             >
+                <li>
+                    <a href="/profile/{data.username}">
+                        <Icon icon="iconamoon:profile-fill" />
+                        Perfil
+                    </a>
+                </li>
                 <li>
                     <a href="/logout" class="text-error">
                         <Icon icon="pepicons-pop:leave" />
