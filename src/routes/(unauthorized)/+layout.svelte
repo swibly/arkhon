655c4457cac9fa7e1e2c77997fbbbec391016826
André devkcud Albanese <patommmmm@proton.me>
2024-09-20 12:10:06 -0300

    refactor: Update layout of landing page

--- src/routes/(unauthorized)/+layout.svelte
+++ src/routes/(unauthorized)/+layout.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
     import Icon from '@iconify/svelte';
+    import { page } from '$app/stores';
 </script>
 
-<header class="fixed top-0 flex justify-between w-full p-4">
-    <a href="/">
-        <Icon icon="ic:baseline-home" class="inline size-8" />
-    </a>
+{#if $page.url.pathname !== '/'}
+    <header class="fixed top-0 flex justify-between w-full p-4">
+        <a href="/" class="transition-opacity opacity-50 hover:opacity-100">
+            <Icon icon="ic:baseline-home" class="inline size-8" />
+        </a>
 
-    <label class="swap swap-rotate">
-        <input type="checkbox" class="theme-controller" value="dark" />
+        <label class="transition-opacity swap swap-rotate opacity-50 hover:opacity-100">
+            <input type="checkbox" class="theme-controller" value="dark" />
 
-        <Icon icon="ph:sun-fill" class="swap-off size-8" />
-        <Icon icon="ph:moon-fill" class="swap-on size-8" />
-    </label>
-</header>
+            <Icon icon="ph:sun-fill" class="swap-off size-8" />
+            <Icon icon="ph:moon-fill" class="swap-on size-8" />
+        </label>
+    </header>
+{/if}
 
 <slot />
