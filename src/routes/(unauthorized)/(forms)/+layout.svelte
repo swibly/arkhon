dabff6da72cc53d5fc4ba2ec939c1f86d1bb5822
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-14 03:22:59 -0300

    feat: Add forgot password

--- src/routes/(unauthorized)/(forms)/+layout.svelte
+++ src/routes/(unauthorized)/(forms)/+layout.svelte
@@ -2,6 +2,8 @@
     import { page } from '$app/stores';
     import Logo from '$lib/components/Logo.svelte';
     import Icon from '@iconify/svelte';
+
+    $: path = $page.url.pathname;
 </script>
 
 <main class="grid w-full min-h-screen p-4 place-items-center">
@@ -9,9 +11,11 @@
         <Logo class="mx-auto mix-blend-difference fill-white" />
 
         <p class="w-full my-8 text-3xl font-light text-center text-primary">
-            {#if $page.url.pathname.startsWith('/login')}
+            {#if path.startsWith('/login')}
                 Bem-vindo de volta!
-            {:else if $page.url.pathname.startsWith('/register')}
+            {:else if path.startsWith('/forgot')}
+                Troque sua senha!
+            {:else if path.startsWith('/register')}
                 Comece sua jornada conosco!
             {/if}
         </p>
@@ -19,7 +23,7 @@
         <slot />
 
         <section class="mt-8 text-center">
-            {#if $page.url.pathname.startsWith('/login')}
+            {#if path.startsWith('/login')}
                 <a href="/forgot" class="text-primary">
                     <Icon icon="mdi:lock-reset" class="inline" />
                     Esqueci a senha
@@ -27,6 +31,16 @@
 
                 <p>
                     Ainda não possui uma conta?
+                    <a href="/register" class="text-primary">Registre-se</a>!
+                </p>
+            {:else if path.startsWith('/forgot')}
+                <p>
+                    Lembrou sua senha?
+                    <a href="/login" class="text-primary">Faça login</a>!
+                </p>
+
+                <p>
+                    Veio parar aqui por engano?
                     <a href="/register" class="text-primary">Registre-se</a>!
                 </p>
             {:else if $page.url.pathname.startsWith('/register')}
