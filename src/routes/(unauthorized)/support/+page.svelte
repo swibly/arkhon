d8064f8b80cec7ffef51113bd62d005a3991e0e7
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-15 02:03:05 -0300

    feat: Add the simplest contact page

---
+++ src/routes/(unauthorized)/support/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Logo from '$lib/components/Logo.svelte';
+</script>
+
+<main class="grid w-full min-h-screen p-4 place-items-center">
+    <div class="w-full max-w-2xl space-y-4">
+        <Logo class="mx-auto mix-blend-difference fill-white" />
+
+        <h2 class="text-center text-3xl">Precisa de ajuda?</h2>
+
+        <p class="text-center">
+            Estamos aqui para você! Envie um email para o
+            <a href="mailto:service@swibly.com.br" class="link link-primary">
+                email de contato da Swibly
+            </a>
+            e responderemos o mais rápido possível.
+        </p>
+    </div>
+</main>
