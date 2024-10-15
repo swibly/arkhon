0455e97bb902118da6157fea35e88935be52d7c6
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-14 21:21:10 -0300

    feat: Add settings page

---
+++ src/routes/(authorized)/settings/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import ProfileTab from '$lib/components/settings-tabs/ProfileTab.svelte';
+    import AccountTab from '$lib/components/settings-tabs/AccountTab.svelte';
+    import PrivacyTab from '$lib/components/settings-tabs/PrivacyTab.svelte';
+    import NotificationsTab from '$lib/components/settings-tabs/NotificationsTab.svelte';
+    import SecurityTab from '$lib/components/settings-tabs/SecurityTab.svelte';
+
+    $: tab = $page.url.searchParams.get('tab')!;
+</script>
+
+{#if tab === 'profile'}
+    <ProfileTab />
+{:else if tab === 'account'}
+    <AccountTab />
+{:else if tab === 'privacy'}
+    <PrivacyTab />
+{:else if tab === 'notifications'}
+    <NotificationsTab />
+{:else if tab === 'security'}
+    <SecurityTab />
+{/if}
