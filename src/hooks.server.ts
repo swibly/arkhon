ce4bb20d9665d74a819d5d7e19105656eee94dbe
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-15 01:53:57 -0300

    feat: Made TOS available at all times

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,20 +2,23 @@ import { redirect, type Handle } from '@sveltejs/kit';
 import { JWT_TOKEN_COOKIE_NAME } from '$env/static/private';
 
 export const handle: Handle = async function ({ event, resolve }) {
-    const UNAUTHORIZED_ROUTES = ['/login', '/register', '/tos', '/forgot'];
+    const UNAUTHORIZED_ROUTES = ['/login', '/register', '/forgot'];
+    const AVAILABLE_ALL_TIMES_ROUTES = ['/tos'];
 
     const token = event.cookies.get(JWT_TOKEN_COOKIE_NAME);
 
     if (
         event.url.pathname === '/' ||
-        UNAUTHORIZED_ROUTES.some((path: string) => event.url.pathname.startsWith(path))
+        !AVAILABLE_ALL_TIMES_ROUTES.some((path: string) => event.url.pathname.startsWith(path))
     ) {
-        if (token) {
-            throw redirect(302, '/home');
-        }
-    } else {
-        if (!token) {
-            throw redirect(302, '/login');
+        if (UNAUTHORIZED_ROUTES.some((path: string) => event.url.pathname.startsWith(path))) {
+            if (token) {
+                throw redirect(302, '/home');
+            }
+        } else {
+            if (!token) {
+                throw redirect(302, '/login');
+            }
         }
     }
 
