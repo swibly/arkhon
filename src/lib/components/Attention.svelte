6af767cba742a63b3168ba70c130a4d6978fff2f
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-14 21:18:30 -0300

    feat: Add attention block component

---
+++ src/lib/components/Attention.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+
+    export let danger: boolean = false;
+</script>
+
+{#if danger === false}
+    <article
+        class="border-l-2 border-l-warning-content p-2 flex items-center gap-2 text-warning-content bg-warning rounded-r-md"
+    >
+        <Icon icon="mdi:alert" class="shrink-0" />
+        <p>
+            <slot />
+        </p>
+    </article>
+{:else}
+    <article
+        class="border-l-2 border-l-error p-2 flex items-center gap-2 text-error bg-error-content rounded-r-md"
+    >
+        <Icon icon="akar-icons:stop-fill" class="shrink-0" />
+        <p>
+            <slot />
+        </p>
+    </article>
+{/if}
