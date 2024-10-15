5802efa0a84729e38b45d670ae29ca30faef8900
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-15 14:29:40 -0300

    chore: Remove useless h1

--- src/lib/components/settings-tabs/PrivacyTab.svelte
+++ src/lib/components/settings-tabs/PrivacyTab.svelte
@@ -42,118 +42,114 @@
     }
 </script>
 
-<section>
-    <h1 class="text-2xl font-bold mb-4">Privacidade do Perfil</h1>
+<form
+    action="/settings?/updatePrivacy"
+    method="POST"
+    class="flex flex-col gap-2"
+    use:enhance={async function () {
+        loading = true;
 
-    <form
-        action="/settings?/updatePrivacy"
-        method="POST"
-        class="flex flex-col gap-2"
-        use:enhance={async function () {
-            loading = true;
+        return ({ result, update }) => {
+            // @ts-ignore
+            handleSavePrivacy(result.data);
 
-            return ({ result, update }) => {
-                // @ts-ignore
-                handleSavePrivacy(result.data);
+            return update({ reset: false });
+        };
+    }}
+>
+    <label for="profile" class="flex items-center gap-2">
+        <input
+            type="checkbox"
+            id="profile"
+            name="profile"
+            checked={user.show.profile}
+            class="checkbox checkbox-xs checkbox-primary"
+        />
+        Exibir perfil publicamente
+        <span
+            class="badge tooltip"
+            data-tip={'Independente das outras permissões, se desativar esta, seu perfil irá aparecer como "não encontrado" para os outros'}
+        >
+            Modificador global
+        </span>
+    </label>
 
-                return update({ reset: false });
-            };
-        }}
-    >
-        <label for="profile" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="profile"
-                name="profile"
-                checked={user.show.profile}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
-            Exibir perfil publicamente
-            <span
-                class="badge tooltip"
-                data-tip={'Independente das outras permissões, se desativar esta, seu perfil irá aparecer como "não encontrado" para os outros'}
-            >
-                Modificador global
-            </span>
-        </label>
+    <label for="projects" class="flex items-center gap-2">
+        <input
+            type="checkbox"
+            id="projects"
+            name="projects"
+            checked={user.show.projects}
+            class="checkbox checkbox-xs checkbox-primary"
+        />
+        Exibir projetos
+        <span
+            class="badge tooltip"
+            data-tip="Seu projeto ainda pode ser visto na comunidade (se estiver público)"
+        >
+            Aplicavel apenas para o perfil
+        </span>
+    </label>
 
-        <label for="projects" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="projects"
-                name="projects"
-                checked={user.show.projects}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
-            Exibir projetos
-            <span
-                class="badge tooltip"
-                data-tip="Seu projeto ainda pode ser visto na comunidade (se estiver público)"
-            >
-                Aplicavel apenas para o perfil
-            </span>
-        </label>
+    <label for="favorites" class="flex items-center gap-2">
+        <input
+            type="checkbox"
+            id="favorites"
+            name="favorites"
+            checked={user.show.favorites}
+            class="checkbox checkbox-xs checkbox-primary"
+        />
+        Exibir favoritos
+    </label>
 
-        <label for="favorites" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="favorites"
-                name="favorites"
-                checked={user.show.favorites}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
-            Exibir favoritos
-        </label>
+    <label for="components" class="flex items-center gap-2">
+        <input
+            type="checkbox"
+            id="components"
+            name="components"
+            checked={user.show.components}
+            class="checkbox checkbox-xs checkbox-primary"
+        />
+        Exibir componentes
+        <span
+            class="badge tooltip"
+            data-tip="Seu componente ainda pode ser visto na comunidade (se estiver público)"
+        >
+            Aplicavel apenas para o perfil
+        </span>
+    </label>
 
-        <label for="components" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="components"
-                name="components"
-                checked={user.show.components}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
-            Exibir componentes
-            <span
-                class="badge tooltip"
-                data-tip="Seu componente ainda pode ser visto na comunidade (se estiver público)"
-            >
-                Aplicavel apenas para o perfil
-            </span>
-        </label>
+    <label for="followers" class="flex items-center gap-2">
+        <input
+            type="checkbox"
+            id="followers"
+            name="followers"
+            checked={user.show.followers}
+            class="checkbox checkbox-xs checkbox-primary"
+        />
+        Exibir seguidores
+    </label>
 
-        <label for="followers" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="followers"
-                name="followers"
-                checked={user.show.followers}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
-            Exibir seguidores
-        </label>
+    <label for="following" class="flex items-center gap-2">
+        <input
+            type="checkbox"
+            id="following"
+            name="following"
+            checked={user.show.following}
+            class="checkbox checkbox-xs checkbox-primary"
+        />
+        Exibir seguindo
+    </label>
 
-        <label for="following" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="following"
-                name="following"
-                checked={user.show.following}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
-            Exibir seguindo
-        </label>
-
-        {#if loading}
-            <button type="button" class="mt-4 btn btn-sm btn-disabled w-fit">
-                <span class="loading loading-spinner loading-md" />
-                Carregando...
-            </button>
-        {:else}
-            <button type="submit" class="mt-4 btn btn-sm btn-primary w-fit">
-                <Icon icon="mdi:feather" />
-                Salvar
-            </button>
-        {/if}
-    </form>
-</section>
+    {#if loading}
+        <button type="button" class="mt-4 btn btn-sm btn-disabled w-fit">
+            <span class="loading loading-spinner loading-md" />
+            Carregando...
+        </button>
+    {:else}
+        <button type="submit" class="mt-4 btn btn-sm btn-primary w-fit">
+            <Icon icon="mdi:feather" />
+            Salvar
+        </button>
+    {/if}
+</form>
