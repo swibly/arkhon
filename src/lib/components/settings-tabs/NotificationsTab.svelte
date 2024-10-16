742f43d8746b628545251e7c6bec18dd67d07996
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-16 20:15:14 -0300

    chore(qol-privacy): Replicate previous QOL in privacy section

--- src/lib/components/settings-tabs/NotificationsTab.svelte
+++ src/lib/components/settings-tabs/NotificationsTab.svelte
@@ -3,10 +3,9 @@
     import { spawn } from '$lib/toast';
     import type { User } from '$lib/user';
     import Icon from '@iconify/svelte';
-
-    let loading = false;
-
-    export let user: User;
+    import Attention from '../Attention.svelte';
+    import Input from '../Input.svelte';
+    import QuestionMark from '../QuestionMark.svelte';
 
     type ActionResult =
         | {
@@ -22,6 +21,10 @@
               error?: undefined;
           }
         | null;
+
+    let loading = false;
+
+    export let user: User;
 
     function handleSaveNotifications(data: ActionResult) {
         loading = false;
@@ -58,41 +61,33 @@
             };
         }}
     >
-        <label for="inapp" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="inapp"
-                name="inapp"
-                checked={user.notification.inapp}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
+        <Input name="inapp" element="checkbox" size="xs" bind:checked={user.notification.inapp}>
             Dentro da plataforma
-        </label>
+            <QuestionMark>
+                Receber notificações direto do portal na seção de notificações (abrindo o menu de
+                usuário).
+            </QuestionMark>
+        </Input>
 
-        <label for="email" class="flex items-center gap-2">
-            <input
-                type="checkbox"
-                id="email"
-                name="email"
-                checked={user.notification.email}
-                class="checkbox checkbox-xs checkbox-primary"
-            />
+        <Input name="email" element="checkbox" size="xs" bind:checked={user.notification.email}>
             Por email
-            <span
-                class="badge tooltip"
-                data-tip="Enviaremos coisas importantes, tipo tentativas de login, mudanças na sua conta etc."
-            >
-                Relaxa, não vamos spammar!
-            </span>
-        </label>
+            <QuestionMark>
+                Enviaremos APENAS coisas importantes, tipo tentativas de login, mudanças na sua
+                conta etc.
+            </QuestionMark>
+        </Input>
+
+        <div class="my-4">
+            <Attention>Não se esqueça de salvar as alterações!</Attention>
+        </div>
 
         {#if loading}
-            <button type="button" class="mt-4 btn btn-sm btn-disabled w-fit">
+            <button type="button" class="btn btn-sm btn-disabled w-fit">
                 <span class="loading loading-spinner loading-md" />
                 Carregando...
             </button>
         {:else}
-            <button type="submit" class="mt-4 btn btn-sm btn-primary w-fit">
+            <button type="submit" class="btn btn-sm btn-primary w-fit">
                 <Icon icon="mdi:feather" />
                 Salvar
             </button>
