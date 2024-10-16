d2f5cc9b8d25ccae7e026e05414665bc8ddd25b1
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-16 19:13:21 -0300

    feat: Add new components

---
+++ src/lib/components/NewBadge.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Icon from '@iconify/svelte';
+</script>
+
+<span class="badge badge-primary badge-outline gap-1">
+    <Icon icon="entypo:new" />
+    Novo
+</span>
---
+++ src/lib/components/QuestionMark.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let tip: string;
+    export let displayText: string = '';
+</script>
+
+<div contenteditable="true" bind:textContent={tip} hidden>
+    <slot />
+</div>
+
+{#if displayText === ''}
+    <span
+        class="tooltip size-4 rounded-full text-xs text-primary-content bg-primary flex justify-center items-center"
+        data-tip={tip}
+    >
+        ?
+    </span>
+{:else}
+    <span class="badge badge-primary badge-sm tooltip" data-tip={tip}>
+        {displayText}
+    </span>
+{/if}
