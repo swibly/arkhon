ce17a12b33c8d94edcdc886a385a27da07879e70
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-17 22:27:42 -0300

    feat: Raise pagination limit

--- src/routes/(authorized)/profile/[username]/favorites/+page.svelte
+++ src/routes/(authorized)/profile/[username]/favorites/+page.svelte
@@ -2,6 +2,7 @@
     import type { User } from '$lib/user';
     import type { PageServerData } from './$types';
     import ProjectCard from '$lib/components/ProjectCard.svelte';
+    import Pagination from '$lib/components/Pagination.svelte';
 
     export let data: PageServerData & User & { lookup: User };
 
@@ -19,35 +20,7 @@
 {:else if projects.length === 0}
     <p class="mb-4 opacity-70 italic">Este usuário ainda não favoritou nenhum projeto.</p>
 {:else}
-    {#if pagination.total_pages !== 1}
-        <div class="mb-4 join">
-            {#if pagination.current_page > 2}
-                <a href="?page=1" class="join-item btn btn-sm">1</a>
-                <button class="join-item btn btn-sm btn-disabled">...</button>
-            {/if}
-
-            {#if pagination.previous_page !== -1}
-                <a href="?page={pagination.previous_page}" class="join-item btn btn-sm">
-                    {pagination.previous_page}
-                </a>
-            {/if}
-
-            <button class="join-item btn btn-sm btn-primary">{pagination.current_page}</button>
-
-            {#if pagination.next_page !== -1}
-                <a href="?page={pagination.next_page}" class="join-item btn btn-sm">
-                    {pagination.next_page}
-                </a>
-            {/if}
-
-            {#if pagination.current_page < pagination.total_pages - 1}
-                <button class="join-item btn btn-sm btn-disabled">...</button>
-                <a href="?page={pagination.total_pages}" class="join-item btn btn-sm">
-                    {pagination.total_pages}
-                </a>
-            {/if}
-        </div>
-    {/if}
+    <Pagination {pagination} />
 
     <div class="grid grid-cols-3 gap-4 max-lg:grid-cols-1 max-md:place-items-center">
         {#each projects as project}
