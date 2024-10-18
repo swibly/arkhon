5b3b1075456f4c824c6f8be96bcc3db0f5eaf030
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-18 17:03:02 -0300

    feat: Add function `getPaginationOptionsFromURL`

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
+    import { page } from '$app/stores';
     import type { Pagination } from '$lib/utils';
 
     export let pagination: Pagination<any>;
+
+    function setupURL(i: number) {
+        let limit = 15;
+        let urlLimit = $page.url.searchParams.get('limit');
+
+        if (urlLimit) {
+            try {
+                limit = parseInt(urlLimit);
+            } catch (e) {
+                limit = 15;
+            }
+        }
+
+        return `?page=${i}&limit=${limit}`;
+    }
 </script>
 
 {#if pagination.total_pages !== 1}
     <div class="mb-4 join">
         {#if pagination.current_page > 2}
-            <a href="?page=1" class="join-item btn btn-sm">1</a>
+            <a href={setupURL(1)} class="join-item btn btn-sm">1</a>
             <button class="join-item btn btn-sm btn-disabled">...</button>
         {/if}
 
         {#if pagination.previous_page !== -1}
-            <a href="?page={pagination.previous_page}" class="join-item btn btn-sm">
+            <a href={setupURL(pagination.previous_page)} class="join-item btn btn-sm">
                 {pagination.previous_page}
             </a>
         {/if}
@@ -20,14 +36,14 @@
         <button class="join-item btn btn-sm btn-primary">{pagination.current_page}</button>
 
         {#if pagination.next_page !== -1}
-            <a href="?page={pagination.next_page}" class="join-item btn btn-sm">
+            <a href={setupURL(pagination.next_page)} class="join-item btn btn-sm">
                 {pagination.next_page}
             </a>
         {/if}
 
         {#if pagination.current_page < pagination.total_pages - 1}
             <button class="join-item btn btn-sm btn-disabled">...</button>
-            <a href="?page={pagination.total_pages}" class="join-item btn btn-sm">
+            <a href={setupURL(pagination.total_pages)} class="join-item btn btn-sm">
                 {pagination.total_pages}
             </a>
         {/if}
