7158f79540adda3ca5155bb4eb1bec570dd4d700
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-26 21:20:46 -0300

    fix: Make text black on warning

--- src/routes/(authorized)/community/projects/[project]/+layout.svelte
+++ src/routes/(authorized)/community/projects/[project]/+layout.svelte
@@ -6,7 +6,9 @@
 </script>
 
 {#if data.project.deleted_at !== null}
-    <p class="p-2 w-full bg-warning flex justify-center items-center gap-2 sticky top-[85px]">
+    <p
+        class="p-2 w-full bg-warning text-black flex justify-center items-center gap-2 sticky top-[85px]"
+    >
         <Icon icon="mdi:alert" />
         Este projeto está na lixeira do dono. Você pode estar vendo uma versão antiga.
     </p>
