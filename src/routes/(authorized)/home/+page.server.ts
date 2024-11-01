276d863ac12dcb2bd564a07040ce1884cad5c807
André devkcud Albanese <patommmmm@proton.me>
2024-11-01 02:04:36 -0300

    fix: Change from searchUsersByName to searchUsers

--- src/routes/(authorized)/home/+page.server.ts
+++ src/routes/(authorized)/home/+page.server.ts
@@ -1,5 +1,5 @@
 import { JWT_TOKEN_COOKIE_NAME } from '$env/static/private';
-import { changeUserImage, deleteUserImage, searchUsersByName } from '$lib/user';
+import { changeUserImage, deleteUserImage, searchUsers } from '$lib/user';
 import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
@@ -8,10 +8,14 @@ export const actions: Actions = {
         const data = await request.formData();
 
         const jwt = cookies.get(JWT_TOKEN_COOKIE_NAME)!;
-        return await searchUsersByName(jwt, data.get('search')?.toString() ?? '', {
-            page: parseInt(data.get('page')?.toString() ?? '1'),
-            limit: parseInt(data.get('limit')?.toString() ?? '3')
-        });
+        return await searchUsers(
+            jwt,
+            { name: data.get('search')?.toString() ?? '' },
+            {
+                page: parseInt(data.get('page')?.toString() ?? '1'),
+                limit: parseInt(data.get('limit')?.toString() ?? '3')
+            }
+        );
     },
     changeImage: async function ({ cookies, request }) {
         const data = await request.formData();
