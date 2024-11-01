54e972d2548b3b3a49af9e58556eb3568e3c4cd8
André devkcud Albanese <patommmmm@proton.me>
2024-10-31 22:51:16 -0300

    chore!: Remove temporary components

--- src/routes/(authorized)/community/components/+page.svelte
+++ src/routes/(authorized)/community/components/+page.svelte
@@ -1,27 +1,10 @@
-<script lang="ts">
-    import type { User } from '$lib/user';
-    import type { PageServerData } from './$types';
-    import Pagination from '$lib/components/Pagination.svelte';
-    import ComponentCard from '$lib/components/ComponentCard.svelte';
-
-    export let data: PageServerData & { user: User };
-
-    $: pagination = data.components;
-    $: components = pagination.data;
+<script>
+    import Attention from '$lib/components/Attention.svelte';
 </script>
 
-{#if components.length === 0}
-    <p class="mb-4 opacity-70 italic">Nenhum projeto encontrado.</p>
-{:else}
-    <div class="mx-auto w-fit">
-        <Pagination {pagination} />
-    </div>
-
-    <div
-        class="grid grid-cols-[repeat(auto-fill,minmax(112px,1fr))] place-items-center grid-flow-dense gap-x-2 gap-y-4"
-    >
-        {#each components as component}
-            <ComponentCard name={component.name} content={component.content} />
-        {/each}
-    </div>
-{/if}
+<div class="max-w-screen-md mx-auto">
+    <Attention>
+        Esta pagina está em construção e ainda não está disponível ao público. Utilize a loja dentro
+        do editor para alterar seus componentes.
+    </Attention>
+</div>
---
+++ src/routes/(authorized)/community/store/+server.ts
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async function () {
+    throw redirect(302, `/community/components`);
+};
