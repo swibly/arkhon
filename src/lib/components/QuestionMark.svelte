bef7f74e38d0148aa37a30c423cf2fbd63cb0803
André devkcud Albanese Junior <patommmmm@proton.me>
2024-11-06 18:02:02 -0300

    fix: Make tooltip be on top

--- src/lib/components/QuestionMark.svelte
+++ src/lib/components/QuestionMark.svelte
@@ -5,13 +5,13 @@
 
 {#if displayText === ''}
     <span
-        class="tooltip size-4 rounded-full text-xs text-primary-content bg-primary inline-flex justify-center items-center cursor-default"
+        class="tooltip size-4 rounded-full text-xs text-primary-content bg-primary inline-flex justify-center items-center cursor-default z-50"
         data-tip={tip}
     >
         ?
     </span>
 {:else}
-    <span class="badge badge-primary badge-sm tooltip" data-tip={tip}>
+    <span class="badge badge-primary badge-sm tooltip z-50" data-tip={tip}>
         {displayText}
     </span>
 {/if}
