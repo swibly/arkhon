3c3d44af84faa20f6afb42070fa00209d4f6c77a
André devkcud Albanese Junior <patommmmm@proton.me>
2024-11-15 00:22:50 -0300

    feat: Better user icons

--- src/lib/components/UserIcon.svelte
+++ src/lib/components/UserIcon.svelte
@@ -5,20 +5,22 @@
     let overflow = users.length - limit;
 </script>
 
-<div class="-space-x-4 avatar-group">
+<div class="-space-x-4 avatar-group overflow-visible">
     {#if overflow > 0}
-        <div class="avatar placeholder">
-            <div class="bg-neutral text-neutral-content w-10 hover:z-50">
+        <div class="avatar placeholder hover:z-50 hover:border-4 hover:border-base-100">
+            <div class="bg-neutral text-neutral-content w-10">
                 <span class="text-xs">+{overflow}</span>
             </div>
         </div>
     {/if}
 
     {#each users.slice(0, limit) as user}
-        <div class="avatar size-12">
-            <a href={`/profile/${user.name}`} class="w-full hover:z-50">
-                <img src={user.pfp} alt="" />
-            </a>
+        <div class="tooltip tooltip-secondary text-xs size-12" data-tip="@{user.name}">
+            <div class="avatar transition size-12 hover:z-50 hover:scale-110 hover:border-4 hover:border-base-100">
+                <a href={`/profile/${user.name}`} class="w-full">
+                    <img src={user.pfp} alt="" />
+                </a>
+            </div>
         </div>
     {/each}
 </div>
