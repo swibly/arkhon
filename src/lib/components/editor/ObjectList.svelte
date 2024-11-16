af29bd984901a59627d1a133ad780d46d145a25c
André devkcud Albanese Junior <patommmmm@proton.me>
2024-11-16 14:51:01 -0300

    fix: Add overflow so it doesnt break canvas

--- src/lib/components/editor/ObjectList.svelte
+++ src/lib/components/editor/ObjectList.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div bind:this={element} class="pr-4 border-r border-base-200 w-60 shrink-0">
-    <ul class="menu menu-sm rounded-lg w-full max-w-xs max-h-full overflow-auto">
+    <ul class="menu flex-nowrap menu-sm rounded-lg w-full max-w-xs max-h-full h-[calc(100vh-89px-49px-2rem)] overflow-auto">
         {#each objects as object}
             <CanvasItem {canvas} {...object} />
         {/each}
